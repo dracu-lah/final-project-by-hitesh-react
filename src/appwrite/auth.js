87960917a7cb08fbb121bf7f498ec57aa9f56cbe
dracu-lah <nevilnicks4321@gmail.com>
2023-11-28 10:53:52 +0530

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .env.example
    	new file:   src/appwrite/auth.js
    	new file:   src/conf/conf.js

---
+++ src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import { Account, Client, ID } from "appwrite";
+import conf from "../conf/conf";
+
+class AuthService {
+  client = new Client();
+  account;
+  constructor() {
+    this.client
+      .setEndpoint(conf.appwriteURL) // Your API Endpoint
+      .setProject(conf.projectID); // Your project ID
+
+    this.account = new Account(this.client);
+  }
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.account.create(
+        ID.unique(),
+        email,
+        password,
+        name,
+      );
+      if (userAccount) {
+        // call another method
+        return this.login({ email, password });
+      } else {
+        return userAccount;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+  async login({ email, password }) {
+    try {
+      return await this.account.createEmailSession(email, password);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getCurrentUser() {
+    try {
+      await this.account.get();
+    } catch (error) {
+      throw error;
+    }
+    return null;
+  }
+  async logout() {
+    try {
+      await this.account.deleteSessions();
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+const authService = new AuthService();
+export default authService;
